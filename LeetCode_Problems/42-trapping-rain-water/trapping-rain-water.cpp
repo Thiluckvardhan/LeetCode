17ca10fbe673c1743428c038aff59b5906bb9d2e
Thiluckvardhan Vemula <122808995+Thiluckvardhan@users.noreply.github.com>
2025-06-24 10:30:00 +0530

    Time: 4 ms (11.80%) | Memory: 28.9 MB (5.02%) - LeetSync

--- LeetCode_Problems/42-trapping-rain-water/trapping-rain-water.cpp
+++ LeetCode_Problems/42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,28 +1,57 @@
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-        int maxwater=0;
-        int n=arr.size();
-        if(n<=2) return 0;
-        int leftglobalmax=arr[0];
-        int rightglobalmax=arr[n-1];
-        int left=0;
-        int right=n-1;
-        while(left<=right){
-            if(arr[left]>leftglobalmax) leftglobalmax=arr[left];
-            if(arr[right]>rightglobalmax) rightglobalmax=arr[right];
-
-            if(arr[left]<=arr[right]){
-                int mint=min(leftglobalmax,rightglobalmax);
-                maxwater+=mint-arr[left];
-                left++;
-            }
-            else{
-                int mint=min(leftglobalmax,rightglobalmax);
-                maxwater+=mint-arr[right];
-                right--;
+    int trap(vector<int>& height) {
+        vector<int>nextgreatest=nextgreatfinder(height);
+        vector<int>prevgreatest=prevgreatfinder(height);
+        int totalwater=0;
+        int n=height.size();
+        if(n<2){
+            return 0;
+        }
+        for(int i=1;i<n-1;i++){
+            int water=min(nextgreatest[i],prevgreatest[i])-height[i];
+            if(water>0){
+                totalwater+=water;
             }
         }
-        return maxwater;
+        return totalwater;
+    }
+    vector<int>nextgreatfinder(vector<int> height){
+        int n=height.size();
+        vector<int>arr(n,-1);
+        stack<int>s;
+        for(int i=n-1;i>=0;i--){
+            int num=height[i];
+            while(!s.empty() && num>=s.top()){
+                s.pop();
+            }
+            if(s.empty()){
+                arr[i]=0;
+                s.push(height[i]);
+            }
+            else{
+                arr[i]=s.top();
+            }
+        }
+        return arr;
+    }
+        vector<int>prevgreatfinder(vector<int> height){
+        int n=height.size();
+        vector<int>arr(n,-1);
+        stack<int>s;
+        for(int i=0;i<n;i++){
+            int num=height[i];
+            while(!s.empty() && num>=s.top()){
+                s.pop();
+            }
+            if(s.empty()){
+                arr[i]=-1;
+                s.push(height[i]);
+            }
+            else{
+                arr[i]=s.top();
+            }
+        }
+        return arr;
     }
 };

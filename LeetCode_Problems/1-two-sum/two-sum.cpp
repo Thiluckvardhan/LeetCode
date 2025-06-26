8970e6cacf408fd698fa5eb51b6f4b35ca173bc6
Thiluckvardhan Vemula <122808995+Thiluckvardhan@users.noreply.github.com>
2025-06-26 11:25:02 +0530

    Time: 3 ms (70.97%) | Memory: 16.6 MB (7.12%) - LeetSync

---
+++ LeetCode_Problems/1-two-sum/two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>hashmap;
+        for(int i=0;i<nums.size();i++)
+        {
+            hashmap[nums[i]]=i;
+        }
+        for(int i=0;i<nums.size();i++){
+            int a=nums[i];
+            int b=target-a;
+            if(hashmap[b] && i!=hashmap[b]){
+                return {i,hashmap[b]};
+                break;
+            }
+        }
+        return {0,0};
+    }
+};

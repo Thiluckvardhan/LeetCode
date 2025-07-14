a149f7b98295438576713a9eb476b73c7aa23125
Thiluckvardhan Vemula <122808995+Thiluckvardhan@users.noreply.github.com>
2025-07-14 10:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (5.80%) - LeetSync

--- LeetCode_Problems/35-search-insert-position/search-insert-position.cpp
+++ LeetCode_Problems/35-search-insert-position/search-insert-position.cpp
@@ -3,10 +3,8 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int low=0;
         int high=nums.size()-1;
-        int mid=0;
-        int index=0;
         while(low<=high){
-            mid=low+(high-low)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target){
                 return mid;
             }

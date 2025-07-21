c0041e4b56d8c2dac61c286d546017680283d7f5
Thiluckvardhan Vemula <122808995+Thiluckvardhan@users.noreply.github.com>
2025-07-21 09:58:15 +0530

    Time: 9 ms (51.40%) | Memory: 39.3 MB (6.01%) - LeetSync

--- LeetCode_Problems/907-koko-eating-bananas/koko-eating-bananas.cpp
+++ LeetCode_Problems/907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1;
-        int high = *max_element(piles.begin(), piles.end());
-        int result = high;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            long hours = findhours(piles, mid);
-
-            if (hours <= h) {
-                result = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        int result=high;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long hours=findhours(piles,mid);
+            if(hours<=h){
+                result=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
-
         return result;
     }
-
-    long findhours(vector<int>& piles, int k) {
-        long count = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            count += (piles[i] + k - 1) / k; 
+    long findhours(vector<int>arr,int n){
+        long hours=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]%n==0){
+                hours+=arr[i]/n;
+            }
+            else{
+                hours+=(arr[i]/n)+1;
+            }
         }
-        return count;
+        return hours;
     }
 };
